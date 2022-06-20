f1d5cc3039d39c2fc68a9749efec3f3bf482c1bb
iobaidu5 <i.obaidu5@gmail.com>
2022-06-20 20:56:18 +0500

    Initial Commit:

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("li.active").removeClass("active");
+    $('a[href="' + location.pathname + '"]')
+      .closest("li")
+      .addClass("active");
+});
+
+const nav = document.querySelector('.fixedNav');
+window.addEventListener('scroll', fixNav);
+
+    new BeerSlider(document.getElementById('slider'));
+
+    $.fn.BeerSlider = function (options) {
+        options = options || {};
+        return this.each(function () {
+            new BeerSlider(this, options);
+        });
+    };
+    $('.beer-slider').BeerSlider({
+        start: 50
+    });
+
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('navbar-active');
+    } else {
+        nav.classList.remove('navbar-active');
+    }
+}
